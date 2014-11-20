0bd7cc4ac995c21d1c9e93ceb3ca29aa14bde4f9
jozwright <jozwright@gmail.com>
2014-11-19 20:51:53 -0800

    allow students to be added

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,7 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    { "name":"Tyra Banks" },
-    { "name":"Fidel Castro" },
-    { "name":"George Lucas" },
-    { "name":"Frida Kahlo" },
-    { "name":"Betty Davis" }
-  ]
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({"name": $scope.studentName});
+    $scope.studentName = null;
+  };
 });
