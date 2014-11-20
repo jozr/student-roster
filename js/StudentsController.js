41a10d628b4c91909e63d011ac11da77c8af0da7
jozwright <jozwright@gmail.com>
2014-11-19 20:47:13 -0800

    add controller

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { "name":"Tyra Banks" },
+    { "name":"Fidel Castro" },
+    { "name":"George Lucas" },
+    { "name":"Frida Kahlo" },
+    { "name":"Betty Davis" }
+  ]
+});
